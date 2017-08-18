b7722e14ced54d2938ed506da9070fc712744cc5
peter soleeh <petersoleeh@gmail.com>
2017-08-18 17:27:48 +0300

    js scripts user input and callback

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,35 @@
+//business logic
+function pingpong(number) {
+  var result = [];
+  for (var i = 1; i <= number; i++) {
+  console.log (result);
+
+    if ((i % 3 === 0 )&& (i % 5 === 0)) {
+      result.push('pingpong');
+    }
+    else if (i % 3 === 0) {
+      result.push('ping');
+    }
+    else if (i % 5 === 0) {
+      result.push('pong')
+    }
+    else {
+      result.push(i);
+    }
+    console.log (result);
+  };
+  return result;
+};
+
+
+// user interface logic
 $(document).ready(function() {
     $("form#ping-pong").submit(function(event) {
       event.preventDefault();
       var number = parseInt($("input#number").val());
       var result = pingpong(number);
+      console.log (pingpong(number));
       $("#result").text(result);
 
     });
   });
-
-    for (var i = 1; i < number.length; i++) {
-      number[i]
-      if (i % 3 === 0 && i % 5 === 0) {
-        console.log('pingpong');
-      }
-      else if (i % 3 === 0) {
-        console.log('ping');
-      }
-      else if (i % 5 === 0) {
-        console.log('pong')
-      }
-      else {
-        console.log(i);
-      }
-
-};
